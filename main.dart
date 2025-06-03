ddc52db68cc70a7f5684b8c91af24f23dd3130c2
Marcus Schmidt <Nightmare240219871987@gmail.com>
2025-06-03 11:04:22 +0200

    first commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+    String gStr = "Guten";
+    String tStr = "Tag";
+    String wStr = "Welt";
+    String zStr = "Zeit";
+    String emStr = "!";
+    String sStr = "Schöne";
+    String mStr = "Morgen";
+    String hStr = "Heute";
+    String eStr = "";
+    int n1Int = 20;
+    int n2Int = 7;
+    int n3Int = 12;
+    int n4Int = 24;     
+}
